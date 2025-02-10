c6a476abeebf72c0c7722528facf5aac2538fa67
Davidnino21 <davidnino_21@yahoo.com>
2025-02-10 06:42:52 -0800

    added date picker and time zones

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,47 @@
 import { useState } from "react";
-
+import moment from "moment-timezone";
 import "./App.css";
 
 function App() {
+  const [time, setTime] = useState("");
+  const [fromZone, setFromZone] = useState("");
+  const [toZone, setToZone] = useState("");
+  const [convertedTime, setConvertedTime] = useState("");
+
+  const timeZones = moment.tz.names();
+
+
+
   return (
     <div className="content">
       <div>
         <h1>Time Zone Transformer</h1>
       </div>
       <div className="card-content">
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
+        <label>Enter Time:</label>
+        <input type="datetime-local" />
+        <label>From Time Zone:</label>
+        <select>
+          {timeZones.map((zone) => (
+            <option key={zone} value={zone}>
+              {zone}
+            </option>
+          ))}
+        </select>
+        <label>To Time Zone:</label>
+        <select>
+          {timeZones.map((zone) => (
+            <option key={zone} value={zone}>
+              {zone}
+            </option>
+          ))}
+          <option value=""></option>
+        </select>
         <div>
           <button>Transform</button>
         </div>
         <div>
-          <p>Your time has been converted</p>
+          <p>Converted Time:</p>
         </div>
       </div>
     </div>
