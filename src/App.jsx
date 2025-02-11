0ae8db858aca9a3bd7badd9a57bbca0a2a2f3820
Davidnino21 <davidnino_21@yahoo.com>
2025-02-11 06:33:20 -0800

    added handle events and click event

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,10 @@ function App() {
 
   const timeZones = moment.tz.names();
 
-
+  function convert() {
+    const m = moment.tz(time, fromZone)
+    setConvertedTime(m.tz(toZone).format('MM/DD/YYYY, h:mm:ss a'));
+  }
 
   return (
     <div className="content">
@@ -19,9 +22,13 @@ function App() {
       </div>
       <div className="card-content">
         <label>Enter Time:</label>
-        <input type="datetime-local" />
+        <input
+          type="datetime-local"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+        />
         <label>From Time Zone:</label>
-        <select value={fromZone}>
+        <select value={fromZone} onChange={(e) => setFromZone(e.target.value)}>
           {timeZones.map((zone) => (
             <option key={zone} value={zone}>
               {zone}
@@ -29,19 +36,18 @@ function App() {
           ))}
         </select>
         <label>To Time Zone:</label>
-        <select value={toZone}>
+        <select value={toZone} onChange={(e) => setToZone(e.target.value)}>
           {timeZones.map((zone) => (
             <option key={zone} value={zone}>
               {zone}
             </option>
           ))}
-          <option value=""></option>
         </select>
         <div>
-          <button>Transform</button>
+          <button onClick={convert}>Transform</button>
         </div>
         <div>
-          <h3>Converted Time:</h3>
+          <h3>Converted Time: {convertedTime}</h3>
         </div>
       </div>
     </div>
